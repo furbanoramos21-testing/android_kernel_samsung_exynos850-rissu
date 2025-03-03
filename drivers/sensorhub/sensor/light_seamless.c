b18bca4a7ac354fbd9d54cdbf4e6153022d7baf7
rsuntk <rissu.ntk@gmail.com>
2025-03-03 15:37:53 +0000

    drivers: [sensorhub, fbdev-dpu30]: Import from A12s
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/sensorhub/sensor/light_seamless.c
+++ drivers/sensorhub/sensor/light_seamless.c
@@ -21,33 +21,29 @@
 
 #include <linux/slab.h>
 
-#define INPUT_MAX 256
-
 static int inject_light_seamless_additional_data(char *buf, int count)
 {
-	int ret = 0;
+	char *token;
+	char *str = NULL;
+    int ret = 0;
 	int index = 0;
 	int thd[2] = {200, 1000000};
-	char input[INPUT_MAX] = {0,};
-	char *input_tmp = NULL, *token = NULL;
 
-	if (count > INPUT_MAX) {
-		shub_errf("bufsize too long(%d)", count);
+	if (count < 4) {
+		shub_errf("length error %d", count);
 		return -EINVAL;
 	}
 
-	memcpy(input, buf, count);
-	input_tmp = input;
+	str = kstrdup(buf, GFP_KERNEL);
+	while ((token = strsep(&str, ",")) != NULL && index < 2) {
+        ret = kstrtoint(token, 10, &thd[index++]);
+        if (ret < 0) {
+            shub_errf("%s - kstrtoint failed.(%d)\n", __func__, ret);
+            return ret;
+        }
+    }
 
-	while ((token = strsep(&input_tmp, ",")) != NULL && index < 2) {
-		ret = kstrtoint(token, 10, &thd[index++]);
-		if (ret < 0) {
-			shub_errf("%s - kstrtoint failed.(%d)\n", __func__, ret);
-			return ret;
-		}
-	}
-
-	shub_info("%s - thd[0] = %d thd[1] = %d", __func__, thd[0], thd[1]);
+    shub_info("%s - thd[0] = %d thd[1] = %d", __func__, thd[0], thd[1]);
 
 	ret = shub_send_command(CMD_SETVALUE, SENSOR_TYPE_LIGHT_SEAMLESS, LIGHT_SUBCMD_THRESHOLD, (char *)&thd, sizeof(thd));
 
