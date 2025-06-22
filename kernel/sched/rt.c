caa8a7b06cc0dfa64742bd90195f8a43f1bd73a3
rsuntk <rissu.ntk@gmail.com>
2025-06-22 09:51:59 +0000

    sched/rt: Revert _pick_next_task_rt to stock
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1637,8 +1637,10 @@ static struct task_struct *_pick_next_task_rt(struct rq *rq)
 
 	do {
 		rt_se = pick_next_rt_entity(rq, rt_rq);
-		if (unlikely(!rt_se))
-			return NULL;
+		BUG_ON(!rt_se);
+		if (rt_se->on_rq)
+			frt_update_load_avg(rt_rq, rt_se, 0);
+		rt_rq->curr = rt_se;
 		rt_rq = group_rt_rq(rt_se);
 	} while (rt_rq);
 
