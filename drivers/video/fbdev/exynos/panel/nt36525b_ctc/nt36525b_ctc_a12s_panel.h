62afc351065bc4d090b4cc43c2b327ac4f5772d7
rsuntk <rissu.ntk@gmail.com>
2025-07-27 16:03:08 +0000

    Revert "drivers: fbdev/exynos/panel: Fix error: expected ';' after top level declarator"
    This reverts commit 442e88af692f793c3a5acb38406d5393c5bd6116.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/video/fbdev/exynos/panel/nt36525b_ctc/nt36525b_ctc_a12s_panel.h
+++ drivers/video/fbdev/exynos/panel/nt36525b_ctc/nt36525b_ctc_a12s_panel.h
@@ -1726,7 +1726,7 @@ static void *a12s_set_bl_cmdtbl[] = {
 static void *a12s_display_on_cmdtbl[] = {
 	&PKTINFO(a12s_display_on),
 	&PKTINFO(a12s_brightness_mode),
-};
+}
 
 static void *a12s_display_off_cmdtbl[] = {
 	&PKTINFO(a12s_display_off),
